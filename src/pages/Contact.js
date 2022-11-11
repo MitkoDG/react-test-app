8b147c8d3eec42fe5835152cd72c1b64de462822
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-11-11 17:06:18 +0200

    added DUMMY text and added Form page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,46 @@
 import React from 'react'
 
-function Contact() {
-    return (
+const ContactForm = () => {
+  const [formStatus, setFormStatus] = React.useState('Send')
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setFormStatus('Submitting...')
+    const { name, email, message } = e.target.elements
+    let conFom = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    }
+    console.log(conFom)
+  }
+  return (
+    <div>
+      <h2>Contact Form</h2>
+      <form onSubmit={onSubmit}>
         <div>
-            <h1>This is the contact page</h1>
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input type="text" id="name" required />
         </div>
-    )
+        <div>
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <input type="email" id="email" required />
+        </div>
+        <div>
+          <label htmlFor="message">
+            Message
+          </label>
+          <textarea id="message" required />
+        </div>
+        <button type="submit">
+          {formStatus}
+        </button>
+      </form>
+    </div>
+  )
 }
 
-export default Contact
+export default ContactForm

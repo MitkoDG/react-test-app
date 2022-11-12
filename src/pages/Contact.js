966f077e1ac4fcb41a6691746ddcd5ebc1a6a5c9
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-11-13 00:02:28 +0200

    update navigation css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -40,6 +40,9 @@ const Contact = () => {
           {formStatus}
         </button>
       </form>
+      {/* <div>
+        <img src='https://images.unsplash.com/photo-1587560699334-bea93391dcef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='Contact Us' width={'300px'}></img>
+      </div> */}
     </div>
   )
 }

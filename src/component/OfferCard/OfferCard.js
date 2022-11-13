2beee3ffce535bc70dc4e0a35daeabd122ab894f
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-11-13 17:22:18 +0200

    render the offers from a json file

--- src/component/OfferCard/OfferCard.js
+++ src/component/OfferCard/OfferCard.js
@@ -1,18 +1,17 @@
-// import IMG from 'https://images.unsplash.com/photo-1527264935190-1401c51b5bbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 import classes from './OfferCard.module.css'
 
-const OfferCard = () => {
+const OfferCard = (props) => {
 
     return (
         <div className={classes.card}>
             <div className={classes.card_header}>
                 <img src='https://images.unsplash.com/photo-1527264935190-1401c51b5bbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='Special deal'></img>
-                <h4 className={classes.header_title}>NEW</h4>
+                {/* <h4 className={classes.header_title}>NEW</h4> */}
             </div>
             <div className={classes.card_body}>
                 <p className={classes.date}>Special deal end soon</p>
-                <h2 className={classes.offer_name}>Basic</h2>
-                <p className={classes.offer_body}>the deal include the full servese for a whole day</p>
+                <h2 className={classes.offer_name}>{props.name}</h2>
+                <p className={classes.offer_body}>{props.description}</p>
                 <button>Buy NOW</button>
             </div>
         </div>
--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import OfferCard from '../component/OfferCard/OfferCard'
 import classes from './Offers.module.css'
+import deals from '../assets/data/deals.json'
+
 const Offers = () => {
+
     return (
         <>
-            <div>
-                <h1>This is the offers page</h1>
+            <div className={classes.head_container}>
+                <h1>Premium Service Improves Customer Experience For All</h1>
             </div>
             <div className={classes.offers}>
-                <OfferCard />
-                <OfferCard />
-                <OfferCard />
+            {deals
+                .map(deal => <OfferCard key={deal.id} name={deal.name} description={deal.description} />)}
             </div>
         </>
     )
